9ef0dea10e90a2a7ea7f37608f5dae6b12afa588
Mariya2603 <maria.web2603@gmail.com>
2021-09-14 17:16:52 +0600

    add jquery

--- js/main.js
+++ js/main.js
@@ -1,13 +1,76 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
+/*document.addEventListener("DOMContentLoaded", function(event) { 
   const modal = document.querySelector('.modal');
+  const modalDialog = $('.modal__dialog');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close');
   const switchModal = () => {
     modal.classList.toggle('modal--visible');
   }
+  const closeModal = () => {
+    modal.classList.remove('modal--visible');
+  }
   
   modalBtn.forEach(element => {
     element.addEventListener('click', switchModal);
   });
+
   closeBtn.addEventListener('click', switchModal);
+
+  document.addEventListener('keyup', function (e) {
+    if (e.key === "Escape") {
+      closeModal();
+    }
+  });
+
+  modal.addEventListener('click', function (e) {
+    if (e.target != modalDialog[0] && modalDialog.has(e.target).length === 0) {
+      closeModal();
+    } 
+  });
+}); */
+
+
+$(document).ready(function () {
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle=modal]'),
+      closeBtn = $('.modal__close');
+      modalDialog = $('.modal__dialog');
+  const closeModal = () => {
+        modal.removeClass('modal--visible');
+      }
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+  $(document).on('keyup', function (e) {
+    if (e.key === "Escape") {
+      closeModal();
+    }
+  });
+
+  modal.on('click', function (e) {
+    if (e.target != modalDialog[0] && modalDialog.has(e.target).length === 0) {
+      closeModal();
+    } 
+  });
+
+  $(window).on('scroll', function(){
+    var control = $('.control').offset().top;
+    if ( $(window).scrollTop() > control) {
+      $('.hero__scroll-up').addClass('active');
+    } else {
+      $('.hero__scroll-up').removeClass('active');
+    }
+  });
+    
+
+  $('.hero__scroll-up').on('click', function(e){
+    e.preventDefault();
+    $('html,body').stop().animate({
+      scrollTop: 0
+    }, 1000);
+  });
+  
 });
